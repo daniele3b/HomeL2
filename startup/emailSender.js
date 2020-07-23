82736dcc655f81e28a33f81297027671fc36e20f
daniele3b <bufalieri.1794424@studenti.uniroma1.it>
2020-07-23 14:43:12 +0200

    added setup email

--- startup/emailSender.js
+++ startup/emailSender.js
@@ -1,13 +1,15 @@
 const nodemailer = require("nodemailer");
+const config = require("config");
+require("dotenv").config();
 
 let transporter = nodemailer.createTransport({
-    host: "smtp.libero.it",
-    port: 465,
-    secure: true, 
-    auth: {
-      user: "", 
-      pass: ""
-    }
+  host: "smtp.libero.it",
+  port: 465,
+  secure: true,
+  auth: {
+    user: process.env.USER_EMAIL,
+    pass: process.env.PW_EMAIL,
+  },
 });
 
-exports.transporter = transporter
+exports.transporter = transporter;
