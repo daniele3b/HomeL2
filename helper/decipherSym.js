aaa47e62262c6f05568fe8be80f0a8c14a31c405
daniele3b <bufalieri.1794424@studenti.uniroma1.it>
2020-08-01 20:10:02 +0200

    fixed dechiper

--- helper/decipherSym.js
+++ helper/decipherSym.js
@@ -17,24 +17,4 @@ function decipherSym(data) {
   return decrypted;
 }
 
-function cipherSym(data) {
-  const algorithm = config.get("algorithm_cipher");
-  const password = process.env.PW_CIPHER;
-
-  const key = crypto.scryptSync(password, "salt", 24);
-
-  // The IV is usually passed along with the ciphertext.
-  const iv = Buffer.alloc(16, 0); // Initialization vector.
-
-  const cipher = crypto.createCipheriv(algorithm, key, iv);
-
-  let encrypted = cipher.update(
-    Buffer.from(JSON.stringify(data), "utf8", "hex")
-  );
-  encrypted += cipher.final("hex");
-
-  return encrypted;
-}
-
-exports.cipherSym = cipherSym;
 exports.decipherSym = decipherSym;
