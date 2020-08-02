e74c96f48fbf5336277353a73142d87f3ad8ce4d
Ivan Giacomoni <giacomoni.1796069@studenti.uniroma1.it>
2020-08-02 13:05:55 +0200

    fixed

--- routes/getStatusServer.js
+++ routes/getStatusServer.js
@@ -1,8 +1,12 @@
 const express = require("express");
 const router = express.Router();
+const config = require("config")
+require("dotenv").config()
 
 router.get("/getStatusServer", (req, res) => {
-    res.status(200).send("Server online!")
+    
+    if(process.env.ASYM_ENC_ACTIVE == "yes") res.status(200).send({encoding: "asymmetric"})
+    else res.status(200).send({encoding: "symmetric"})
 });
 
 module.exports = router;
