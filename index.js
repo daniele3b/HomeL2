833def63723ec2ab1411924be5e04a130ac01d97
Ivan Giacomoni <giacomoni.1796069@studenti.uniroma1.it>
2020-09-06 09:59:16 +0200

    setted some configuration's parameters

--- index.js
+++ index.js
@@ -3,29 +3,33 @@ const { GenerateTemplate } = require("./startup/templateLoader");
 
 const config = require("config");
 
-if (config.get("blockChainActive") == "yes") {
-  var cors = require("cors");
-  const { mine } = require("./helper/mine");
-  const port = config.get("port");
-  const express = require("express");
-  var app = express();
-  var bodyParser = require("body-parser");
+var cors = require("cors");
+const port = config.get("port");
+const express = require("express");
+var app = express();
+var bodyParser = require("body-parser");
 
-  app.use(cors());
-  //setting bodyparser
-  app.use(bodyParser.json({ limit: "50mb" }));
-  app.use(
-    bodyParser.urlencoded({
-      limit: "50mb",
-      extended: true,
-      parameterLimit: 50000,
-    })
-  );
-  //setting routes blockchain
-  require("./startup/routes")(app);
-  app.listen(port, function () {
-    console.log("Listening on port " + port + "...");
-  });
+app.use(cors());
+//setting bodyparser
+app.use(bodyParser.json({ limit: "50mb" }));
+app.use(
+  bodyParser.urlencoded({
+    limit: "50mb",
+    extended: true,
+    parameterLimit: 50000,
+  })
+);
+
+require("./startup/routes")(app);
+
+app.listen(port, function () {
+  console.log("Listening on port " + port + "...");
+});
+
+
+
+if (config.get("blockChainActive") == "yes") {
+  const { mine } = require("./helper/mine");
 
   //set time mine
   setInterval(() => {
--- startup/routes.js
+++ startup/routes.js
@@ -1,13 +1,17 @@
 const Blockchain = require("../blockchain/networkNode");
 const key_RSA_producer = require("../routes/key_RSA_producer");
 const getStatusServer = require("../routes/getStatusServer");
-const bodyParser = require("body-parser");
 const config = require("config");
 
 module.exports = function (app) {
-  app.use("/", Blockchain);
+  if(config.get("blockChainActive") == "yes"){
+    app.use("/", Blockchain);
+  }
+
   app.use("/", getStatusServer);
+
   if (config.get("security_active") == "yes") {
     app.use("/", key_RSA_producer);
   }
+  
 };
